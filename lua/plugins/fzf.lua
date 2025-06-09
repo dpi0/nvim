b177afb8d7ce0fcdc4e4627adb693b627142d694
divyansh <dpi0.dev@proton.me>
2025-06-09 19:37:49 +0530

    Add VeryLazy event to fzf to enable mini.snippets completion

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,6 +1,7 @@
 return {
   'ibhagwan/fzf-lua',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  event = 'VeryLazy',
   config = function()
     require('fzf-lua').register_ui_select()
   end,
