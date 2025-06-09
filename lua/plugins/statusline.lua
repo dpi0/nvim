99e9c5293ae23dfe8dec896eb4fd256544c8c049
divyansh <dpi0.dev@proton.me>
2025-06-09 13:05:08 +0530

    Add essential plugins.mini (statusline, pairs, comments etc)

---
+++ lua/plugins/statusline.lua
@@ -0,0 +1,8 @@
+return {
+  'echasnovski/mini.statusline',
+  -- event = 'VeryLazy',
+  version = '*',
+  -- empty opts table runs the require('mini.surround').setup() automatically
+  opts = {},
+}
+
