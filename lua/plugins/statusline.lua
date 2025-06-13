ae4f21d270982af7e6922ed7ffc59e4cb6173411
divyansh <dpi0.dev@proton.me>
2025-06-13 16:20:38 +0530

    Update mini.statusline with only the required modules

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,7 +1,40 @@
 return {
   'echasnovski/mini.statusline',
-  -- event = 'VeryLazy',
   version = '*',
-  -- empty opts table runs the require('mini.surround').setup() automatically
-  opts = {},
+  config = function()
+    local MiniStatusline = require 'mini.statusline'
+
+    -- Wrap section_mode to return only the first character
+    local function short_mode()
+      local mode_str, mode_hl = MiniStatusline.section_mode { trunc_width = 100 }
+      return mode_str:sub(1, 1), mode_hl
+    end
+
+    local active = function()
+      local mode, mode_hl = short_mode()
+      local git = MiniStatusline.section_git { trunc_width = 40 }
+      local diff = MiniStatusline.section_diff { trunc_width = 75 }
+      local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
+      local lsp = MiniStatusline.section_lsp { trunc_width = 75 }
+      local filename = MiniStatusline.section_filename { trunc_width = 140 }
+      -- local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
+      local location = MiniStatusline.section_location { trunc_width = 75 }
+      local search = MiniStatusline.section_searchcount { trunc_width = 75 }
+
+      return MiniStatusline.combine_groups {
+        { hl = mode_hl, strings = { mode } },
+        { hl = 'MiniStatuslineDevinfo', strings = { git, diff, lsp } },
+        '%<',
+        { hl = 'MiniStatuslineFilename', strings = { filename, search } },
+        '%=',
+        { hl = mode_hl, strings = { diagnostics, location } },
+      }
+    end
+
+    MiniStatusline.setup {
+      content = { active = active },
+      use_icons = true,
+      set_vim_settings = false,
+    }
+  end,
 }
