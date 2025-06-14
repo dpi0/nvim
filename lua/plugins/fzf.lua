65e291578d84733f59b1bf5a3792590c89fa7079
divyansh <dpi0.dev@proton.me>
2025-06-14 20:57:39 +0530

    Update fzf config (overhaul)
    Removed opts and keys lazy.nvim lua tables, added colors and cleaned up
    the config

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,323 +3,176 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   event = 'VeryLazy',
   config = function()
-    local fzf = require 'fzf-lua'
-    -- local actions = require('fzf-lua').actions
-    fzf.register_ui_select()
-    fzf.setup {
+    local actions = require('fzf-lua').actions
+    local opts = {
+      previewers = {
+        builtin = {
+          syntax_limit_b = 1024 * 100,
+        },
+      },
+      hls = {
+        match = 'FzfLuaFzfMatch',
+      },
+      fzf_opts = {
+        ['--cycle'] = true,
+      },
+      winopts = {
+        -- height = 1, -- height
+        -- width = 1, -- width
+        -- row = 0, -- window row position (0=top, 1=bottom)
+        -- col = 0, -- window col position (0=left, 1=right)
+        fullscreen = true,
+        preview = {
+          vertical = 'down:60%', -- show preview at bottom, take 60% of space
+          layout = 'vertical',
+          scrollbar = 'float',
+          scrolloff = '-4',
+        },
+        -- backdrop = 60, -- Backdrop opacity, 0 is fully opaque, 100 is fully transparent (i.e. disabled)
+        border = 'rounded',
+      },
+      grep = {
+        rg_glob = true,
+        glob_flag = '--iglob',
+        glob_separator = '%s%-%-',
+        cmd = 'rg --line-number --column --no-heading --color=always',
+        rg_opts = table.concat({
+          '--hidden',
+          '--no-ignore-vcs',
+          '--glob=!**/.local/**',
+          '--glob=!**/.rustup/**',
+          '--glob=!**/node_modules/**',
+          '--glob=!**/.cargo/**',
+          '--glob=!**/.continue/**',
+          '--glob=!**/.mozilla/**',
+          '--glob=!**/go/pkg/mod/**',
+          '--glob=!**/Code/User/**',
+          '--glob=!**/.git/**',
+          '--glob=!**/.npm/**',
+          '--glob=!**/.cache/**',
+        }, ' '),
+      },
+      files = {
+        cmd = table.concat({
+          'fd --type f --hidden --follow',
+          '--exclude .git',
+          '--exclude node_modules',
+          '--exclude .cargo',
+          '--exclude .mozilla',
+          '--exclude .cache',
+          '--exclude .npm',
+          '--exclude .rustup',
+          '--exclude .dotnet',
+          '--exclude go/pkg/mod',
+          '--exclude Code/User',
+          '--exclude .local',
+          '--exclude .continue',
+        }, ' '),
+        formatter = 'path.dirname_first',
+        -- formatter = 'path.filename_first',
+        cwd_prompt_shorten_len = 50, -- shorten prompt beyond this length
+        cwd_prompt_shorten_val = 3, -- shortened path parts length       cwd_prompt_shorten_len = 50,
+      },
+      defaults = {
+        git_icons = true,
+        file_icons = true,
+        color_icons = true,
+      },
       keymap = {
         builtin = {
+          true,
           ['<C-d>'] = 'preview-page-down',
           ['<C-u>'] = 'preview-page-up',
         },
+        fzf = {
+          true,
+          ['ctrl-d'] = 'preview-page-down',
+          ['ctrl-u'] = 'preview-page-up',
+          ['ctrl-q'] = 'select-all+accept',
+        },
+      },
+      actions = {
+        files = {
+          ['enter'] = actions.file_edit_or_qf,
+          ['ctrl-x'] = actions.file_split,
+          ['ctrl-v'] = actions.file_vsplit,
+          ['ctrl-t'] = actions.file_tabedit,
+          ['alt-q'] = actions.file_sel_to_qf,
+        },
       },
     }
+
+    -- https://github.com/ibhagwan/fzf-lua?tab=readme-ov-file#highlight-groups
+    vim.api.nvim_set_hl(0, 'FzfLuaBorder', { fg = '#54546D' })
+    vim.api.nvim_set_hl(0, 'FzfLuaDirPart', { fg = '#8DA2C2', italic = true })
+    vim.api.nvim_set_hl(0, 'FzfLuaFilePart', { fg = '#90C98E', bold = true })
+
+    local fzf = require 'fzf-lua'
+    fzf.register_ui_select()
+    fzf.setup(opts)
+
+    local bind_opts = { noremap = true, silent = true }
+
+    local bind = function(keys, func, desc, mode)
+      vim.keymap.set(mode or 'n', keys, func, vim.tbl_extend('force', bind_opts, { desc = desc }))
+    end
+
+    bind('<leader>fzf', fzf.builtin, 'fzf ❤️')
+    bind('<leader>ff', fzf.resume, 'Resume Previous fzf Search')
+
+    -- Files
+    bind('<A-f>', fzf.files, 'Find Files')
+    bind('<leader>f/', function()
+      fzf.files { cwd = '/' }
+    end, 'Search root / directory')
+    bind('<leader>fh', function()
+      fzf.files { cwd = os.getenv 'HOME', hidden = true }
+    end, 'Search $HOME directory')
+    bind('<leader>fm', function()
+      fzf.files { cwd = '$HOME/notes' }
+    end, 'Search $HOME/notes directory')
+    bind('<leader>fn', function()
+      fzf.files { cwd = vim.fn.stdpath 'config' }
+    end, 'Search $HOME/.config/nvim directory')
+
+    -- Grep
+    bind('<A-g>', fzf.live_grep, 'Grep')
+    bind('<leader>fM', function()
+      fzf.live_grep_native { cwd = '$HOME/notes' }
+    end, 'Grep Notes')
+    bind('<A-/>', fzf.blines, 'Buffer Lines')
+    bind('<leader>gw', fzf.grep_cword, 'Grep Word')
+    bind('<leader>fc', fzf.grep_cword, 'Grep Word (Alt)')
+    bind('<A-S-f>', fzf.grep_cWORD, 'Grep WORD')
+    bind('<leader>ft', '<cmd>TodoFzfLua<CR>', 'Search TODOs')
+
+    -- Buffers / Recents
+    bind('<leader><leader>', fzf.buffers, 'Buffers')
+    bind('<leader>f,', fzf.oldfiles, 'Recent Files')
+
+    -- Git
+    bind('<leader>gca', fzf.git_commits, 'Git Commits')
+    bind('<leader>gH', fzf.git_bcommits, 'Git Buffer Commits')
+
+    -- Misc
+    bind('<leader>K', fzf.keymaps, 'Keymaps')
+    bind('<leader>fd', fzf.diagnostics_document, 'Document Diagnostics')
+    bind('<leader>fD', fzf.diagnostics_workspace, 'Workspace Diagnostics')
+    bind('<leader>fq', fzf.quickfix, 'Quickfix List')
+    bind('<leader>H', fzf.help_tags, 'Help Tags')
+    bind('<leader>mar', fzf.marks, 'Marks')
+    bind('<leader>o', fzf.zoxide, 'Zoxide')
+    bind('<leader>fs', function()
+      fzf.spell_suggest {
+        winopts = {
+          height = 0.4,
+          width = 0.5,
+          row = 1, -- relative to cursor
+          col = 0,
+          relative = 'cursor',
+          preview = { hidden = 'hidden' },
+        },
+      }
+    end, 'Spelling Suggest (Cursor)')
   end,
-  opts = {
-    previewers = {
-      builtin = {
-        syntax_limit_b = 1024 * 100, -- 100KB limit on previews
-      },
-    },
-    fzf_opts = { ['--cycle'] = true },
-    grep = {
-      rg_glob = true,
-      glob_flag = '--iglob',
-      glob_separator = '%s%-%-',
-      cmd = 'rg --line-number --column --no-heading --color=always',
-      rg_opts = table.concat({
-        '--hidden',
-        '--no-ignore-vcs',
-        '--glob=!**/.local/**',
-        '--glob=!**/.rustup/**',
-        '--glob=!**/node_modules/**',
-        '--glob=!**/.cargo/**',
-        '--glob=!**/.continue/**',
-        '--glob=!**/.mozilla/**',
-        '--glob=!**/go/pkg/mod/**',
-        '--glob=!**/Code/User/**',
-        '--glob=!**/.git/**',
-        '--glob=!**/.npm/**',
-        '--glob=!**/.cache/**',
-      }, ' '),
-    },
-    files = {
-      cmd = table.concat({
-        'fd --type f --hidden --follow',
-        '--exclude .git',
-        '--exclude node_modules',
-        '--exclude .cargo',
-        '--exclude .mozilla',
-        '--exclude .cache',
-        '--exclude .npm',
-        '--exclude .rustup',
-        '--exclude .dotnet',
-        '--exclude go/pkg/mod',
-        '--exclude Code/User',
-        '--exclude .local',
-        '--exclude .continue',
-      }, ' '),
-    },
-    defaults = {
-      git_icons = true,
-      file_icons = true,
-      color_icons = true,
-    },
-  },
-  keys = (function()
-    -- Common window configurations
-    local win_configs = {
-      large = {
-        height = 0.9,
-        width = 0.9,
-        row = 0.5,
-        col = 0.5,
-        anchor = 'center',
-        preview = {
-          layout = 'horizontal',
-          horizontal = 'right:50%',
-        },
-      },
-      vertical = {
-        height = 0.9,
-        width = 0.8,
-        row = 0.5,
-        col = 0.5,
-        anchor = 'center',
-        preview = {
-          layout = 'vertical',
-          vertical = 'up:50%',
-        },
-      },
-      fullscreen = {
-        width = 1.0,
-        height = 1.0,
-        row = 0.0,
-        col = 0.0,
-        anchor = 'NW',
-      },
-      cursor = {
-        relative = 'cursor',
-        row = 1.01,
-        col = 0,
-        width = 0.2,
-        height = 0.2,
-      },
-    }
-
-    -- Helper function to create file finder with custom options
-    local function files_with_opts(opts)
-      return function()
-        require('fzf-lua').files(opts or {})
-      end
-    end
-
-    -- Helper function to create grep with custom options
-    local function grep_with_opts(opts)
-      return function()
-        require('fzf-lua').live_grep(opts or {})
-      end
-    end
-
-    return {
-      -- Core fuzzy finder
-      {
-        '<leader>fzf',
-        function()
-          require('fzf-lua').builtin()
-        end,
-        desc = 'FzfLua ❤️',
-      },
-      {
-        '<leader>ff',
-        function()
-          require('fzf-lua').resume()
-        end,
-        desc = 'Resume last fzf-lua window',
-      },
-
-      -- File finders
-      { '<A-f>', files_with_opts { winopts = win_configs.large }, desc = 'Fuzzy find files in cwd' },
-      { '<leader>f/', files_with_opts { cwd = '/', hidden = true }, desc = 'Find files in /root' },
-      {
-        '<leader>fh',
-        files_with_opts { cwd = os.getenv 'HOME', hidden = true, winopts = win_configs.large },
-        desc = 'Find files in $HOME',
-      },
-      { '<leader>fm', files_with_opts { cwd = '$HOME/notes' }, desc = 'Find files in $HOME/notes' },
-      {
-        '<leader>fn',
-        files_with_opts { cwd = vim.fn.stdpath 'config', winopts = win_configs.large },
-        desc = 'Find files in Neovim config',
-      },
-
-      -- Grep/search
-      {
-        '<A-g>',
-        grep_with_opts {
-          winopts = vim.tbl_extend('force', win_configs.vertical, { fzf_opts = { ['--layout'] = 'reverse' } }),
-          glob_flag = '--iglob',
-        },
-        desc = 'Grep',
-      },
-      {
-        '<leader>fM',
-        function()
-          require('fzf-lua').live_grep_native { cwd = '$HOME/notes' }
-        end,
-        desc = 'Grep in $HOME/notes',
-      },
-      {
-        '<A-/>',
-        function()
-          require('fzf-lua').blines {
-            winopts = vim.tbl_extend('force', win_configs.vertical, { fzf_opts = { ['--layout'] = 'reverse' } }),
-          }
-        end,
-        desc = 'Fuzzy search in current buffer',
-      },
-      {
-        '<leader>gw',
-        function()
-          require('fzf-lua').grep_cword()
-        end,
-        desc = 'Find string in cwd',
-      },
-      {
-        '<leader>fc',
-        function()
-          require('fzf-lua').grep_cword()
-        end,
-        desc = 'Find string under cursor in cwd',
-      },
-      {
-        '<A-S-f>',
-        function()
-          require('fzf-lua').grep_cWORD()
-        end,
-        desc = 'Find string under cursor in cwd',
-      },
-      {
-        '<leader>ft',
-        function()
-          require('fzf-lua').grep { cmd = 'rg --column --line-number', search = 'TODO', prompt = 'Todos> ' }
-        end,
-        desc = 'Find TODOs',
-      },
-
-      -- Buffers and recent files
-      {
-        '<leader><leader>',
-        function()
-          require('fzf-lua').buffers { winopts = win_configs.large }
-        end,
-        desc = 'Find existing buffers',
-      },
-      {
-        '<leader>f,',
-        function()
-          require('fzf-lua').oldfiles()
-        end,
-        desc = 'Fuzzy find recent files',
-      },
-      {
-        '<leader>ro',
-        function()
-          require('fzf-lua').oldfiles { winopts = win_configs.large }
-        end,
-        desc = 'Recent Files',
-      },
-
-      -- Git
-      {
-        '<leader>gca',
-        function()
-          require('fzf-lua').git_commits { winopts = win_configs.fullscreen }
-        end,
-        desc = 'Search Git Commits for cwd',
-      },
-      {
-        '<leader>gH',
-        function()
-          require('fzf-lua').git_bcommits { winopts = win_configs.fullscreen }
-        end,
-        desc = 'Search Git Commits for Current File Only',
-      },
-
-      -- Utilities
-      {
-        '<leader>K',
-        function()
-          require('fzf-lua').keymaps()
-        end,
-        desc = 'Check keymap availability',
-      },
-      {
-        '<leader>ce',
-        function()
-          require('fzf-lua').colorschemes()
-        end,
-        desc = 'Preview Colorschemes',
-      },
-      {
-        '<leader>fd',
-        function()
-          require('fzf-lua').diagnostics_workspace { winopts = win_configs.fullscreen }
-        end,
-        desc = 'Search Diagnostics',
-      },
-      {
-        '<leader>fq',
-        function()
-          require('fzf-lua').quickfix()
-        end,
-        desc = 'Show quick fix list',
-      },
-      {
-        '<leader>fH',
-        function()
-          require('fzf-lua').help_tags {
-            winopts = vim.tbl_extend(
-              'force',
-              win_configs.fullscreen,
-              { preview = { layout = 'vertical', vertical = 'up:80%' } }
-            ),
-          }
-        end,
-        desc = 'Search Help (Fullscreen)',
-      },
-      {
-        '<leader>mar',
-        function()
-          require('fzf-lua').marks { fzf_opts = { ['--query'] = '^' } }
-        end,
-        desc = 'Marks',
-      },
-      {
-        '<leader>o',
-        function()
-          require('fzf-lua').zoxide { winopts = win_configs.fullscreen }
-        end,
-        desc = 'Zoxide switch b/w cwd',
-      },
-
-      -- Spelling and code actions
-      {
-        '<leader>fs',
-        function()
-          require('fzf-lua').spell_suggest { winopts = win_configs.cursor }
-        end,
-        desc = 'Spelling suggestions (Overriden default z=)',
-      },
-      {
-        '<leader>ca',
-        function()
-          require('fzf-lua').lsp_code_actions {
-            winopts = { relative = 'cursor', width = 0.6, height = 0.6, row = 1, preview = { vertical = 'up:70%' } },
-          }
-        end,
-        desc = 'Code actions',
-      },
-    }
-  end)(),
 }
