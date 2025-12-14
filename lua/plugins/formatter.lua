377e0cb789ddcfc5e66a94593590e0f1c15ab25f
divyansh <dpi0.dev@proton.me>
2025-12-14 18:39:11 +0530

    Update formatter

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,44 +1,43 @@
 return {
   'stevearc/conform.nvim',
-  event = { 'BufEnter' },
-  cmd = { 'ConformInfo' },
+  event = 'BufEnter',
+  cmd = 'ConformInfo',
   opts = {
+    format_on_save = {
+      timeout_ms = 2000,
+      lsp_format = 'fallback',
+    },
+    -- Custom formatters
+    formatters = {
+      rumdl = {
+        command = 'rumdl',
+        args = { 'fmt', '-' },
+        stdin = true,
+        env = {
+          RUMDL_CACHE_DIR = vim.fn.expand '~/.cache/rumdl',
+        },
+      },
+    },
     -- https://github.com/stevearc/conform.nvim#formatters
-    -- list of all formatters
-    -- make sure to run :checkhealth conform
-    -- to see whether tools are present or not for formatting
     formatters_by_ft = {
       lua = { 'stylua' },
-      -- python = { "ruff" },
       python = { 'ruff_organize_imports', 'ruff_fix', 'ruff_format' },
-      go = { 'goimports', 'gofmt' },
-      -- yaml = { 'yamlfmt', 'djlint' },
+      go = { 'gofmt' },
       yaml = { 'yamlfmt' },
-      -- yaml = { 'prettierd', 'prettier', stop_after_first = true },
       bash = { 'shfmt' },
       zsh = { 'shfmt' },
       sh = { 'shfmt' },
-      json = { 'prettierd', 'prettier', stop_after_first = true },
-      jsonc = { 'prettierd', 'prettier', stop_after_first = true },
+      json = { 'prettierd' },
+      jsonc = { 'prettierd' },
       toml = { 'taplo' },
-      -- markdown = { 'mdformat', 'cbfmt' },
-      markdown = { 'mdformat' },
-      nix = { 'alejandra' },
+      -- nix = { 'alejandra' },
       terraform = { 'terraform_fmt' },
-      -- You can customize some of the format options for the filetype (:help conform.format)
-      -- rust = { "rustfmt" },
-      -- Conform will run the first available formatter
-      javascript = { 'prettierd', 'prettier', stop_after_first = true },
-      css = { 'prettierd', 'prettier', stop_after_first = true },
-      html = { 'prettierd', 'prettier', stop_after_first = true },
-      -- typescript = { "prettierd", "prettier", stop_after_first = true },
-      -- Use the "*" filetype to run formatters on all filetypes.
+      javascript = { 'prettierd' },
+      css = { 'prettierd' },
+      html = { 'prettierd' },
+      dart = { 'dart_format' },
       ['*'] = { 'trim_newlines', 'trim_whitespace' },
-    },
-    format_on_save = {
-      -- These options will be passed to conform.format()
-      timeout_ms = 500,
-      lsp_format = 'fallback',
+      markdown = { 'rumdl' }, -- Custom formatter defined above
     },
   },
 }
