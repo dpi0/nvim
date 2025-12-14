cde688a7c690ec8588e7d1c98841f5122b902284
divyansh <dpi0.dev@proton.me>
2025-12-14 18:48:50 +0530

    Replace grapple with actual harpoon
    Harpoon feels much snappier & with lastplace plugin it solves the big
    issue for me

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,32 +1,79 @@
 return {
-  'cbochs/grapple.nvim',
-  event = 'VeryLazy',
-  -- event = { 'BufReadPost', 'BufNewFile' },
-  dependencies = {
-    { 'nvim-tree/nvim-web-devicons', lazy = true },
-  },
-  cmd = 'Grapple',
-  opts = {
-    scope = 'cwd', -- global, git, git_branch, cwd, lsp
-    icons = true, -- setting to "true" requires "nvim-web-devicons"
-    status = true,
-  },
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { '<leader>j', '<cmd>Grapple toggle_tags<cr>', desc = 'Toggle tags menu' },
-    { '<leader>p', '<cmd>Grapple toggle<cr>', desc = 'Tag a file' },
-    -- { '<A-e>', '<cmd>Grapple toggle<cr>', desc = 'Tag a file' },
-
-    { '<A-1>', '<cmd>Grapple select index=1<cr>' },
-    { '<A-2>', '<cmd>Grapple select index=2<cr>' },
-    { '<A-3>', '<cmd>Grapple select index=3<cr>' },
-    { '<A-4>', '<cmd>Grapple select index=4<cr>' },
-    { '<A-0>', '<cmd>Grapple select index=5<cr>' },
-    { '<A-9>', '<cmd>Grapple select index=6<cr>' },
-    { '<A-8>', '<cmd>Grapple select index=7<cr>' },
-    { '<A-7>', '<cmd>Grapple select index=8<cr>' },
-    { '<A-6>', '<cmd>Grapple select index=9<cr>' },
-
-    -- { '<c-s-n>', '<cmd>Grapple cycle_tags next<cr>', desc = 'Go to next tag' },
-    -- { '<c-s-p>', '<cmd>Grapple cycle_tags prev<cr>', desc = 'Go to previous tag' },
+    {
+      '<leader>fa',
+      function()
+        require('harpoon'):list():add()
+      end,
+      desc = 'Harpoon: Add file',
+    },
+    {
+      '<leader>j',
+      function()
+        local harpoon = require 'harpoon'
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = 'Harpoon: Toggle quick menu',
+    },
+    {
+      '<A-1>',
+      function()
+        require('harpoon'):list():select(1)
+      end,
+      desc = 'Harpoon: Go to file 1',
+    },
+    {
+      '<A-2>',
+      function()
+        require('harpoon'):list():select(2)
+      end,
+      desc = 'Harpoon: Go to file 2',
+    },
+    {
+      '<A-3>',
+      function()
+        require('harpoon'):list():select(3)
+      end,
+      desc = 'Harpoon: Go to file 3',
+    },
+    {
+      '<A-4>',
+      function()
+        require('harpoon'):list():select(4)
+      end,
+      desc = 'Harpoon: Go to file 4',
+    },
+    {
+      '<A-0>',
+      function()
+        require('harpoon'):list():select(5)
+      end,
+      desc = 'Harpoon: Go to file 5',
+    },
+    {
+      '<A-9>',
+      function()
+        require('harpoon'):list():select(6)
+      end,
+      desc = 'Harpoon: Go to file 6',
+    },
+    -- Toggle previous & next buffers stored within Harpoon list
+    {
+      '<A-S-H>',
+      function()
+        require('harpoon'):list():prev()
+      end,
+      desc = 'Harpoon: Go to previous',
+    },
+    {
+      '<A-S-L>',
+      function()
+        require('harpoon'):list():next()
+      end,
+      desc = 'Harpoon: Go to next',
+    },
   },
 }
