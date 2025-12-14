3a3193d240172c73eb190deea7a8e09cb0a8c4eb
divyansh <dpi0.dev@proton.me>
2025-12-14 18:52:14 +0530

    Update oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,52 +1,66 @@
 return {
   'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
-  config = function()
-    require('oil').setup {
-      -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
-      -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
-      default_file_explorer = true,
-      -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
-      delete_to_trash = true,
-      -- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
-      skip_confirm_for_simple_edits = true,
-      view_options = {
-        show_hidden = true,
-        -- Sort file names with numbers in a more intuitive order for humans.
-        -- Can be "fast", true, or false. "fast" will turn it off for large directories.
-        natural_order = true,
-        -- This function defines what will never be shown, even when `show_hidden` is set
-        is_always_hidden = function(name, _)
-          return name == '..' or name == '.git'
-        end,
-      },
-      float = {
-        padding = 2,
-        max_width = 90,
-        max_height = 0,
-      },
-      win_options = {
-        wrap = true,
-        winblend = 0,
-      },
-      keymaps = {
-        ['<C-c>'] = false,
-        ['q'] = 'actions.close',
-        -- ['<CR>'] = 'actions.select',
-        ['<S-l>'] = 'actions.select',
-        ['<S-h>'] = { 'actions.parent', mode = 'n' },
-      },
-    }
-    -- https://github.com/stevearc/oil.nvim/issues/384#issuecomment-2210882925
-    vim.api.nvim_create_user_command('OilToggle', function()
-      vim.cmd((vim.bo.filetype == 'oil') and 'bd' or 'Oil')
-    end, { nargs = 0 })
-  end,
+  dependencies = { 'nvim-mini/mini.icons', opts = {} },
+  opts = {
+    default_file_explorer = true,
+    watch_for_changes = true,
+    columns = {
+      'icon',
+      -- 'permissions',
+      'size',
+      'mtime',
+    },
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+      show_hidden = true,
+      natural_order = true,
+      is_always_hidden = function(name, _)
+        return name == '..' or name == '.git'
+      end,
+    },
+    float = {
+      padding = 2,
+      max_width = 90,
+      max_height = 0,
+    },
+    win_options = {
+      wrap = true,
+      winblend = 0,
+    },
+    preview_win = {
+      width = 0.9,
+    },
+    use_default_keymaps = true,
+    keymaps = {
+      ['<C-c>'] = false,
+      ['<C-s>'] = false,
+      ['<C-h>'] = false,
+      ['<C-l>'] = false,
+      ['<C-t>'] = false,
+      ['<A-S-j>'] = false,
+      ['<A-S-k>'] = false,
+      ['q'] = 'actions.close',
+      ['<S-l>'] = 'actions.select',
+      ['<S-h>'] = { 'actions.parent', mode = 'n' },
+      ['g?'] = { 'actions.show_help', mode = 'n' },
+      ['<C-p>'] = 'actions.preview',
+      ['<C-r>'] = 'actions.refresh',
+      ['-'] = { 'actions.parent', mode = 'n' },
+      ['_'] = { 'actions.open_cwd', mode = 'n' },
+      ['`'] = { 'actions.cd', mode = 'n' },
+      ['~'] = { 'actions.cd', opts = { scope = 'tab' }, mode = 'n' },
+      ['gs'] = { 'actions.change_sort', mode = 'n' },
+      ['gx'] = 'actions.open_external',
+      ['g.'] = { 'actions.toggle_hidden', mode = 'n' },
+      ['g\\'] = { 'actions.toggle_trash', mode = 'n' },
+      ['<S-k>'] = { 'actions.preview_scroll_up', mode = 'n' },
+      ['<S-j>'] = { 'actions.preview_scroll_down', mode = 'n' },
+    },
+  },
+  -- config = function()
+  --   require('oil').setup {
+  --   }
+  -- end,
 }
