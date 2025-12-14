ecc6674d3789bf25620396ae0e7bb0107e958eb9
divyansh <dpi0.dev@proton.me>
2025-12-14 18:52:29 +0530

    Refactor outline

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -1,10 +1,12 @@
 return {
-  'hedyhli/outline.nvim',
-  lazy = true,
-  cmd = { 'Outline', 'OutlineOpen' },
-  keys = {
-    { '<leader>O', '<cmd>Outline<CR>', desc = 'Toggle outline' },
+  {
+    'hedyhli/outline.nvim',
+    lazy = true,
+    cmd = { 'Outline', 'OutlineOpen' },
+    keys = {
+      { '<leader>O', '<cmd>Outline<CR>', desc = 'Toggle outline' },
+    },
+    -- https://github.com/hedyhli/outline.nvim?tab=readme-ov-file#default-options
+    opts = { width = 20, show_cursorline = false },
   },
-  -- https://github.com/hedyhli/outline.nvim?tab=readme-ov-file#default-options
-  opts = { width = 20, show_cursorline = false },
 }
