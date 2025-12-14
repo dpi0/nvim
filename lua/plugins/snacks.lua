43c0c190863e3165443555a95c52dbc68bcea9bd
divyansh <dpi0.dev@proton.me>
2025-12-14 18:52:51 +0530

    Cleanup snacks and add image and quickfile

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -15,6 +15,12 @@ return {
         },
       },
     },
+    quickfile = {},
+    image = {
+      doc = {
+        -- inline = false, -- Render image only on cursor url hover
+      },
+    },
     input = {},
     -- test with
     -- :lua vim.notify("Test notification from command line")
@@ -25,67 +31,6 @@ return {
       -- editor margin to keep free. tabline and statusline are taken into account automatically
       margin = { top = 0, right = 1, bottom = 0 },
       padding = true, -- add 1 cell of left/right padding to the notification window
-    },
-    -- terminal = { enabled = true },
-    picker = {
-      enabled = false,
-      -- In case you want to make sure that the score manipulation above works
-      -- or if you want to check the score of each file
-      debug = {
-        scores = false, -- show scores in the list
-      },
-      -- I like the "ivy" layout, so I set it as the default globaly, you can
-      -- still override it in different keymaps
-      layout = {
-        preset = 'default',
-        -- When reaching the bottom of the results in the picker, I don't want
-        -- it to cycle and go back to the top
-        cycle = true,
-      },
-      layouts = {
-        -- I wanted to modify the layout width
-        --
-        vertical = {
-          layout = {
-            backdrop = false,
-            width = 0.8,
-            min_width = 80,
-            height = 0.8,
-            min_height = 30,
-            box = 'vertical',
-            border = 'rounded',
-            title = '{title} {live} {flags}',
-            title_pos = 'center',
-            { win = 'input', height = 1, border = 'bottom' },
-            { win = 'list', border = 'none' },
-            { win = 'preview', title = '{preview}', height = 0.4, border = 'top' },
-          },
-        },
-      },
-      matcher = {
-        frecency = true,
-      },
-      win = {
-        input = {
-          keys = {
-            -- to close the picker on ESC instead of going to normal mode,
-            -- add the following keymap to your config
-            ['<Esc>'] = { 'close', mode = { 'n', 'i' } },
-            ['q'] = { 'hide', mode = { 'n', 'i' } },
-            -- I'm used to scrolling like this in LazyGit
-            ['J'] = { 'preview_scroll_down', mode = { 'i', 'n' } },
-            ['K'] = { 'preview_scroll_up', mode = { 'i', 'n' } },
-            ['H'] = { 'preview_scroll_left', mode = { 'i', 'n' } },
-            ['L'] = { 'preview_scroll_right', mode = { 'i', 'n' } },
-          },
-        },
-      },
-      formatters = {
-        file = {
-          -- filename_first = true, -- display filename before the file path
-          truncate = 80,
-        },
-      },
     },
     lazygit = {
       enabled = true,
