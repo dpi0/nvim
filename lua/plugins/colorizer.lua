11e500031fd8c18aa8ce3c6b7d4df0dc5ae6c544
divyansh <dpi0.dev@proton.me>
2025-12-14 18:33:00 +0530

    Add multiple colorscheme options for colorizer

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,5 +1,17 @@
 return {
   'catgoose/nvim-colorizer.lua',
   event = 'BufReadPre',
-  opts = {},
+  opts = {
+    -- https://github.com/catgoose/nvim-colorizer.lua#customization
+    user_default_options = {
+      css = true, -- Enable all CSS *features*:
+      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn, oklch_fn
+      tailwind = true, -- Enable tailwind colors
+      xterm = false, -- Enable xterm 256-color codes (#xNN, \e[38;5;NNNm)
+      mode = 'background', -- Set the display mode
+      virtualtext = '■',
+      virtualtext_inline = false,
+      virtualtext_mode = 'foreground',
+    },
+  },
 }
